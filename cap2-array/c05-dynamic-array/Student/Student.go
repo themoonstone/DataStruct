fd1f6dca4730b0996bf2e3a325968de6dbc6c58c
themoonstone <18611674065@163.com>
2019-03-22 07:31:43 +0800

    动态数组实现

---
+++ cap2-array/c05-dynamic-array/Student/Student.go
@@ -0,0 +1,12 @@
+package Student
+
+import "fmt"
+
+type Student struct {
+	Name string
+	Score int
+}
+
+func (s *Student) String() string{
+	return fmt.Sprintf("Student(name:%s, score:%d)",s.Name, s.Score)
+}
--- cap2-array/c05-dynamic-array/main.go
+++ cap2-array/c05-dynamic-array/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"themoonstone/DataStruct/cap2-array/c04-generic/Student"
-	"themoonstone/DataStruct/cap2-array/c04-generic/array"
+	"themoonstone/DataStruct/cap2-array/c05-dynamic-array/array"
+
+	"themoonstone/DataStruct/cap2-array/c05-dynamic-array/Student"
 )
 func main() {
 	Int()
@@ -13,21 +14,22 @@ func main() {
 func Int()  {
 	var a array.Array
 	a.InitDefaultArray()
-
-	a.AddLast("ls")
-	a.AddFirst(20)
-	a.Insert(1,1)
+	a.AddFirst(100)
+	fmt.Printf("before resize data : %v\n", a.String())
+	for i := 0; i < 20; i++ {
+		a.Insert(i, i)
+	}
 	fmt.Println("size:", a.GetSize())
 	fmt.Printf("data : %v\n", a.String())
 	// 获取指定位置的元素
 	fmt.Printf("before update val : %v\n", a.Get(0))
 	// 设置指定位置的元素
-	a.Set(2,100)
+	a.Set(1,100)
 	fmt.Printf("after update val : %v\n", a.String())
 	a.Remove(0)
 	fmt.Println("after delete val : ", a.String())
-	a.RemoveHead()
-	fmt.Println("after delete head : ", a.String())
+	//a.RemoveHead()
+	//fmt.Println("after delete head : ", a.String())
 	a.RemoveTail()
 	fmt.Println("after delete tail : ", a.String())
 }
@@ -38,8 +40,5 @@ func Stu() {
 	as.AddLast(Student.Student{"troytan",120})
 	as.AddLast(Student.Student{"xiaomei",90})
 	as.AddLast(Student.Student{"alice",105})
-	as.RemoveTail()
-	as.RemoveHead()
-	as.Remove(19)
 	fmt.Println(as.String())
 }
