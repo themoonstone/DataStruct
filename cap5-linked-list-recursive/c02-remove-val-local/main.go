992d7fd43e22c807a02d72a49203fddc5a27bcd1
themoonstone <18611674065@163.com>
2019-03-28 20:48:56 +0800

    为leetcode 203添加本地测试

---
+++ cap5-linked-list-recursive/c02-remove-val-local/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"themoonstone/DataStruct/cap5-linked-list-recursive/c02-remove-val-local/list"
+)
+
+func main() {
+	// 给定一个数组，生成一个链表
+	var nums []int = []int{1,2,6,3,4,5,6}
+	var listNode *list.ListNode = new(list.ListNode)
+
+	listNode.New(nums)
+	fmt.Println(listNode.String())
+
+	fmt.Println(list.RemoveElementsWithDummyHead(listNode, 6).String())
+}
